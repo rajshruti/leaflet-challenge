e3bf19cfe240cfaf9bb68129158b6864a0b04643
rajshruti <shrutiraj.bhu@gmail.com>
2020-02-28 00:05:45 -0500

    Update Leaflet Step-1

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,81 +1,94 @@
-// Creating map object
-var map = L.map("map", {
-  center: [36.7783, -119.4179],
-  zoom: 5
-});
-
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(map);
-
 // Import earthquake dataset
-var link = "static/data/all_week.geojson";
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Function that will determine the color of the markers
-function marker(magnitude) {
+d3.json(link, function(data){
 
-  switch (magnitude) {
-  case magnitude<0:
-    return "#F8F88A";
-  case magnitude<1:
-    return "#FFE173";
-  case magnitude<2:
-    return "#FFC65C";
-  case magnitude<3:
-    return "#FFAA0C";
-  case magnitude<4:
-    return "#FB841B";
-  case magnitude<5:
-    return "#F9743A";
-  case magnitude>=5:
-    return "#FA1405";
-  default:
-    return "#05FAC6";
-  }
+  // Define different map layers
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+  
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+  
+  var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+  
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap,
+    "Satellite": satellite
+  };
+
+function markerSize(magnitude) {
+    return magnitude * 4;
+  };
+
+ // Function that will determine the color of the markers 
+function markerColor(magnitude) {
+    if (magnitude > 5) {
+        return '#FE0422'
+    } else if (magnitude > 4) {
+        return '#FE5704'
+    } else if (magnitude > 3) {
+        return '#FA7A1C '
+    } else if (magnitude > 2) {
+        return '#FABD1C'
+    } else if (magnitude > 1) {
+        return '#FCF105'
+    } else {
+        return '#9BFC05'
+    }
 };
 
-// Grabbing our GeoJSON data.
-d3.json(link, function(data) {
-  // Creating a geoJSON layer with the retrieved data
-  L.geoJson(data, {
-    // Style each feature
-    style: function(feature) {
+var earthquakes=L.geoJSON(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, { radius: markerSize(feature.properties.mag) })
+    }, 
+    style: function (feature) {
       return {
-        color: "white",
-        // Call the chooseColor function to decide which color to color the markers
-        fillColor: marker(properties.mag),
-        fillOpacity: 1.0,
-        weight: 1.5,
-		// Adjust radius
-        radius: properties.mag * 10000 // just for testing; to be adjusted later
-      };
-    },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.9
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.5
-          });
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h1>" + properties.place + "</h1> <hr> <h2>" + properties.mag + "</h2>");
+          fillColor: markerColor(feature.properties.mag),
+          fillOpacity: 0.7,
+          weight: 0.1,
+          color: 'black'
 
-    }
-  }).addTo(map);
+      }
+  },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+          "<h4 style='text-align:center;'>" + new Date(feature.properties.time) +
+          "</h4> <hr> <h5 style='text-align:center;'>" + feature.properties.title + "</h5>");
+  }
+  });
+   
+
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+    ],
+    zoom: 4,
+    layers: [streetmap,earthquakes]
+  });
+
+// Create overlay object to hold our overlay layer
+var overlayMaps = {
+  Earthquakes: earthquakes
+  };
+  
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+  }).addTo(myMap);
 });
